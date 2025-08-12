545a9c2a4b0df798954502ff5c6ad826fb2f94a8
AbhinayaSriRachha <rachhaabhinaya@gmail.com>
2025-08-12 11:41:43 +0530

    commiting abhinaya and main

---
+++ Main.java
@@ -0,0 +1,12 @@
+ public class Main {
+    public static void main(String[] args) {
+        String name = "Abhinaya";
+        int age = 22;
+        double height = 5.6;
+
+        // Using printf for formatted output
+        System.out.printf("Name: %s\n", name);
+        System.out.printf("Age: %d years\n", age);
+        System.out.printf("Height: %.1f feet\n", height);
+    }
+}
